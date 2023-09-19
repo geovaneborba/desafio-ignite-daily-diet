d2f1a7b8c08ba58ddf8895b7cfb3f269af4cf110
Geovane Borba <geovanne@live.com>
2023-09-19 05:32:41 -0300

    chore: add theme style guide to maintain visual consistency

---
+++ src/theme/default.ts
@@ -0,0 +1,37 @@
+export const theme = {
+  colors: {
+    'red-dark': '#BF3B44',
+    'red-mid': '#F3BABD',
+    'red-light': '#F4E6E7',
+
+    'green-dark': '#639339',
+    'green-mid': '#CBE4B4',
+    'green-light': '#E5F0DB',
+
+    'gray-1': '#1B1D1E',
+    'gray-2': '#333638',
+    'gray-3': '#5C6265',
+    'gray-4': '#B9BBBC',
+    'gray-5': '#DDDEDF',
+    'gray-6': '#EFF0F0',
+    'gray-7': '#FAFAFA',
+
+    white: '#FFFFFF',
+  },
+
+  fontFamily: {
+    regular: 'Nunito_400Regular',
+    bold: 'Nunito_700Bold',
+    extraBold: 'MPLUSRounded1c_800ExtraBold',
+  },
+
+  fontSize: {
+    sm: '12px',
+    md: '14px',
+    base: '16px',
+    lg: '18px',
+    xl: '24px',
+    '2xl': '32px',
+    '3xl': '36px',
+  },
+}
