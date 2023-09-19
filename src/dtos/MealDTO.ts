5b2176d1d0dcc870537496d95ee1c6a02ecb6d14
Geovane Borba <geovanne@live.com>
2023-09-19 00:56:47 -0300

    chore: add DTOs to structure transfer data

---
+++ src/dtos/MealDTO.ts
@@ -0,0 +1,9 @@
+export type MealDTO = {
+  id: string | number[]
+  description?: string
+  name: string
+  date: Date
+  hour: Date
+  diet: string
+  created_at: Date
+}
---
+++ src/dtos/MealStatisticDTO.ts
@@ -0,0 +1,7 @@
+export type MealStatisticDTO = {
+  mealsOutsideDiet: number
+  mealsInsideDiet: number
+  totalMeals: number
+  currentSequence: number
+  bestSequence: number
+}
